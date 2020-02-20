f013546a85eb0c53713045c7926c2abd762bc9e1
loiminh <loiho11082015@gmail.com>
2020-02-20 09:53:03 +0700

    add file

---
+++ src/Point3D.java
@@ -0,0 +1,41 @@
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D(){
+
+    }
+    public Point3D(float z, float x, float y){
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        super.setXY(x,y);
+        this.z = z;
+
+    }
+    public float [] getXYZ(){
+        float [] f = new float[3];
+        f[0] = getX();
+        f[1] = getY();
+        f[2] = z;
+        return f;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" +getX() +
+                "y=" +getY() +
+                "z=" + z +
+                '}';
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+        Point2D point = new Point2D();
+        System.out.println(point);
+
+        point = new Point2D(3, 4);
+        System.out.println(point);
+
+
+
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+        point = new Point3D(3,6,5);
+        System.out.println(point);
+
+    }
+}
